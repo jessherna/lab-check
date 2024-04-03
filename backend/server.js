b58538ca472f1cb62909f0654b4189b28fcc312f
Jessica Hernandez <jessicamariep.hernandez@gmail.com>
2024-04-03 00:53:57 -0400

    put back port 5000

--- backend/server.js
+++ backend/server.js
@@ -98,6 +98,6 @@ io.on('connection', (socket) => {
   });
 });
 
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`Server started on port ${port}`));
